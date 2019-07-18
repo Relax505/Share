1086f2d0eabb079e399c039ab8197ef86383062f
Relax505 <45914158+Relax505@users.noreply.github.com>
2019-07-18 08:37:35 +0300

    Update Sharing-BOT.js

--- Sharing-BOT.js
+++ Sharing-BOT.js
@@ -1,14 +1,14 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 let timer;
-console.log("BOT ONLINE");
+console.log("love you discord");
 
 client.on("guildMemberAdd", member => {
     timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
   let words = [‏ستمرّ عليك لحظاتٌ تستطيعُ فيها فعل كلّ شيء.. هذه ليست قوة، إنما مقدرة، القوةُ تأتي من اللحظاتِ التي ظننتَ أنك لن تتمكن من فعل شيءٍ فيها، وأنها ستهزمكَ لا محالة، ثمّ انتصرتَ عليها!
 
-سيرفر Harmony عوده حميده بإذن الله.. :emhar:
-                               [https://discord.gg/YE2DMsC]
+مرحباً أو مُرحباً Tactful.
+                               [https://discord.gg/amtnCCq]
 ... [ ${member}  
   setTimeout(() =>{
   member.createDM().then(function (channel) {
@@ -17,4 +17,4 @@ client.on("guildMemberAdd", member => {
 }, timer)
 })
 
-client.login("NTI2NTQwMjM0NDM1OTg1NDM4.DyYoNw.CCuKki_nsgOW-3gXn2GnR5QmaEg") 
+client.login("NjAxMjgxNTc0NzI1NzQ2Njg5.XTABNw.FU864IxuZ12IfQe6w0GGTj9VBaE") 
